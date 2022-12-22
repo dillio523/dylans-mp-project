48725a06dbf60f312f16ae44cc5909c6a395e062
dylan deehan <dylan_deehan@msn.com>
2022-12-22 17:24:04 +0000

    model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_21_094635) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_22_130823) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "constituencies", force: :cascade do |t|
+    t.string "name"
+    t.integer "constituency_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "members", force: :cascade do |t|
     t.datetime "created_at", null: false
